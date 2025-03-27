a4bad8899c910ced2fdeb1beeb68a618b517d27b
Murat ALBAYRAK <muratalbayrak236@gmail.com>
2025-03-28 00:44:04 +0300

    feature: add data automation
    verilerin filtrelenmesi, paslarin, sutlarin ve skorlarin eklenmesi otomatik hala getirildi. Tum maclar tek seferde calistirabilir hale geldi.

---
+++ data_result/Automation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import csv
+import json
+from pathlib import Path
+import numpy as np
+import Filter_Code.find_passes as find_passes
+
+game_ids = {'21500001', "21500002"}
+
+for game_id in game_ids:
+    find_passes.update_json_with_actions(game_id)
---
+++ data_result/Filter_Code/find_score.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def find_score_result(game_id):
+    # Dosya yolu
+    game_id = int(game_id)
+    file_path = "Row_data/shots_fixed.csv"
+
+    # CSV dosyasını yükle
+    df_original = pd.read_csv(file_path)
+
+    # İlgili maç ID'sini filtrele
+    df_filtered = df_original[df_original["GAME_ID"] == game_id]
+
+    # Başarılı şutları filtrele (SHOT_ATTEMPTED_FLAG = 1 ve SHOT_MADE_FLAG = 1 olanlar)
+    df_made_shots = df_filtered[(df_filtered["SHOT_ATTEMPTED_FLAG"] == 1) & (df_filtered["SHOT_MADE_FLAG"] == 1)]
+
+    # Şutları zaman sırasına göre sırala
+    df_made_shots = df_made_shots.sort_values(by=["PERIOD", "MINUTES_REMAINING", "SECONDS_REMAINING"], ascending=[True, False, False])
+
+    # Ev sahibi ve rakip takımı belirle
+        # Eğer filtrelenmiş DataFrame boşsa, hata vermeden None döndür
+    if df_filtered.empty:
+        print(f"Hata: {game_id} ID'li maç için veri bulunamadı!")
+        return None  # Veya uygun bir varsayılan değer
+    
+    home_team = df_filtered.iloc[0]["TEAM_NAME"]
+    away_team = df_filtered[df_filtered["TEAM_NAME"] != home_team].iloc[0]["TEAM_NAME"]
+
+    # Skor değişimlerini hesapla
+    score_progression = []
+
+    for _, row in df_made_shots.iterrows():
+        team = row["TEAM_NAME"]
+        points = 3 if "3PT" in row["SHOT_TYPE"] else 2  # 3'lük veya 2'lik şut kontrolü
+        game_clock = row["MINUTES_REMAINING"] * 60 + row["SECONDS_REMAINING"]  # Oyun saati (saniye cinsinden)
+
+        # Basketi atan takımın ev sahibi mi rakip mi olduğunu belirle
+        is_home_team = 1 if team == home_team else 0
+
+        # Yeni formatta listeye ekle
+        score_progression.append(f"{row['PERIOD']},{int(game_clock)},{is_home_team},{points}")
+
+
+
+    # Güncellenmiş veriyi dosyaya kaydet
+    #output_file_path = "Data/fixed_shots/score_result.csv"
+
+    #with open(output_file_path, "w") as file:
+        #file.write("\n".join(score_progression))
+
+    # Dosya yolu döndürme
+    #output_file_path
+    return "\n".join(score_progression)
+
+
+if __name__ == "__main__":
+    game_id = "21500491"
+    find_score_result(game_id)
