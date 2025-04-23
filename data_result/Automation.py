338d94ffe2eea2f9d96a9b5878b59587ff3328a9
Kadir Pekdemir <116032771+KadirPekdemir@users.noreply.github.com>
2025-04-23 20:53:36 +0300

    all data filtered

--- data_result/Automation.py
+++ data_result/Automation.py
@@ -5,7 +5,13 @@ from pathlib import Path
 import numpy as np
 import Filter_Code.find_passes as find_passes
 
-game_ids = {'21500001', "21500002", "21500491"}
+# CSV dosyasını oku
+df = pd.read_csv('Row_data/shots_fixed.csv')
+
+#game_ids = {'21500001', "21500002", "21500491"}
+
+# Benzersiz GAME_ID'leri al
+game_ids = df['GAME_ID'].unique()
 
 for game_id in game_ids:
     find_passes.update_json_with_actions(game_id)
